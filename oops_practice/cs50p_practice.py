bb11038f72394ee23899387f3f68d34ddd137c98
Shreyansh Rastogi <shreyanshrastogi8@gmail.com>
2026-02-25 13:51:11 +0530

    added __sts__ func in class

--- oops_practice/cs50p_practice.py
+++ oops_practice/cs50p_practice.py
@@ -1,19 +1,36 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name: str, house: str):
+        if not name:
+            raise ValueError("missing name.")
+        if house not in ["negi", "rastogi"]:
+            raise ValueError("invalid house")
+
         self.name = name
         self.house = house
 
-
-def student_getter():
-    name = input("enter name:")
-    house = input("input house:")
-
-    return Student(name, house)
+    def __str__(self) -> str:
+        return f"i do some bullshit"
 
 
-def main():
+def student_getter() -> Student:
+    while True:
+        name = input("name:")
+        house = input("house:")
+        try:
+            student = Student(name, house)
+        except ValueError as e:
+
+            print(e)
+        else:
+            break
+
+    return student
+
+
+def main() -> None:
     student = student_getter()
     print(f"{student.name} is from {student.house}")
+    print(student)
 
 
 if __name__ == "__main__":
