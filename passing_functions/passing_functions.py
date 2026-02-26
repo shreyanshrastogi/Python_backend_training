c840065ebbd676c1268f78299748651f48b09dcd
Shreyansh Rastogi <shreyanshrastogi8@gmail.com>
2026-02-26 09:36:59 +0530

    added small comment

--- passing_functions/passing_functions.py
+++ passing_functions/passing_functions.py
@@ -1,16 +1,18 @@
+
 def decorator(func):
+    #wrapper around the function
     def wrapper():
         print("code before the func executes")
         func()
         print("code after the func executes")
-
+    #returning wrapped function
     return wrapper
 
-
+# independent function
 def heloo():
     print("hello")
 
-
+# decorating the function
 function = decorator(heloo)
-
+# calling new function which refers to wrapper inside decoraotor which contains our function heloo with some other code before and after it
 function()
