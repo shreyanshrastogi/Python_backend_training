b565d6c7dd6fb8ceb1f63799a1f2e665d19a8b13
Shreyansh Rastogi <shreyanshrastogi8@gmail.com>
2026-02-26 10:58:56 +0530

    made a get requests api functions in Fastapi

--- the_first_api/main.py
+++ the_first_api/main.py
@@ -10,7 +10,19 @@ def status() -> dict[str, str]:
     return stat
 
 
-@app.get("/prof")
+@app.get("/profile")
 def profile():
     details = {"username": "admin", "role": "python backend developer"}
     return details
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int) -> dict[str, int | str]:
+    id = {"requested_id": user_id, "status": "active"}
+    return id
+
+
+@app.get("/items")
+def read_items(skip: int = 0, limit: int = 10) -> dict[str, int]:
+    item = {"skip_value": skip, "limit_value": limit}
+    return item
