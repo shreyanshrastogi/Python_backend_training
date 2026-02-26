64dd85667c1ee0fb722683ca959c2c45441266cb
Shreyansh Rastogi <shreyanshrastogi8@gmail.com>
2026-02-26 15:31:59 +0530

    learned the usage of query parameter

--- The_CRUD_api/main.py
+++ The_CRUD_api/main.py
@@ -6,37 +6,32 @@ inventory = {}
 
 
 class Item(BaseModel):
-    item_id: int
     item: str
     quantity: int
     price: float
 
 
-@app.post("/inventory/add")
-def add_item(item_data: Item):
-    if item_data.item_id in inventory:
-        raise HTTPException(status_code=404, detail="item already present")
-    inventory[item_data.item_id] = {}
-    inventory[item_data.item_id]["item"] = item_data.item
-    inventory[item_data.item_id]["quantity"] = item_data.quantity
-    inventory[item_data.item_id]["price"] = item_data.price
-    return inventory
+@app.post("/inventory/add/{item_id}")
+def add_item(item_data: Item, item_id: int):
+    if item_id in inventory:
+        raise HTTPException(status_code=409, detail="item already present")
+    inventory[item_id] = item_data
+    return {"message": "added", "item": inventory[item_id]}
 
 
 @app.get("/inventory/check/{item_id}")
 def check_item(item_id: int):
     if item_id not in inventory:
-        raise HTTPException(status_code=409, detail="item not present")
+        raise HTTPException(status_code=404, detail="item not present")
     return inventory[item_id]
 
 
 @app.put("/inventory/update/{item_id}")
-def update_item(item_id: int, item_data: Item):
+def update_item(item_data: Item, item_id: int):
     if item_id not in inventory:
         raise HTTPException(status_code=404, detail="item not present")
-    inventory[item_id]["item"] = item_data.item
-    inventory[item_id]["quantity"] = item_data.quantity
-    inventory[item_id]["price"] = item_data.price
+    inventory[item_id] = item_data
+    return {"message": "updated", "item": inventory[item_id]}
 
 
 @app.delete("/inventory/delete/{item_id}")
@@ -44,3 +39,4 @@ def delete_item(item_id: int):
     if item_id not in inventory:
         raise HTTPException(status_code=404, detail="item not present")
     del inventory[item_id]
+    return {"message": f"{item_id} deleted successfully"}
