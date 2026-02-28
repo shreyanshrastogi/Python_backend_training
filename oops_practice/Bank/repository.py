71f601a85e703054abaf87c8609f38d0a44618a8
Shreyansh Rastogi <shreyanshrastogi8@gmail.com>
2026-02-28 13:49:08 +0530

    built a repository file with class DB having feature of adding deleting and checking accounts

---
+++ oops_practice/Bank/repository.py
@@ -0,0 +1,33 @@
+import models
+
+
+class DB:
+    def __init__(self):
+        self.__db = {}
+
+    def add_account(self, account: models.Account) -> dict[str, str]:
+        if account.account_id in self.__db:
+            raise ValueError("account_id already exist")
+        self.__db[account.account_id] = account
+        return {"message": f"{account.account_id} added successfully"}
+
+    def get_account(self, account_id: int) -> models.Account:
+        if account_id not in self.__db:
+            raise ValueError("account not available")
+
+        return self.__db[account_id]
+
+    def get_all_accounts(self) -> dict[int, models.Account]:
+        return self.__db
+
+    def delete_account(self, account_id: int) -> dict[str, str]:
+        if account_id not in self.__db:
+            raise ValueError("account not found")
+        del self.__db[account_id]
+        return {"message": f"{account_id} deleted successfully"}
+
+    def account_exists(self, account_id: int) -> bool:
+        if account_id in self.__db:
+            return True
+        else:
+            return False
