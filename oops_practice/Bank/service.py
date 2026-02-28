a864c4ad367fbbfa80fce9084d109daaa0f74792
Shreyansh Rastogi <shreyanshrastogi8@gmail.com>
2026-02-28 17:04:24 +0530

    made a advance BankService class that is using other classes in models and repository file and learned how they can use each other even if they are in other file

--- oops_practice/Bank/service.py
+++ oops_practice/Bank/service.py
@@ -1,2 +1,41 @@
 import models
 import repository
+
+
+class BankServices:
+
+    def __init__(self, db: repository.AccountDB):
+        self.db = db
+
+    def create_account(self, name: str, account_id: int, balance: float):
+        account: models.Account = models.Account(
+            name=name, account_id=account_id, balance=balance
+        )
+        self.db.add_account(account)
+
+    def deposit(self, account_id: int, amount: float):
+
+        self.db.get_account(account_id=account_id).deposit(amount)
+
+    def withdraw(self, account_id: int, amount: float):
+        self.db.get_account(account_id=account_id).withdraw(amount)
+
+    def transfer(
+        self, sender_id: int, receiver_id: int, amount: float
+    ) -> dict[str, str | models.Account]:
+        self.db.get_account(sender_id).withdraw(amount)
+        self.db.get_account(receiver_id).deposit(amount)
+        return {
+            "message": f"{amount} transferred successfully",
+            "from": self.db.get_account(sender_id),
+            "To": self.db.get_account(receiver_id),
+        }
+
+    def get_balance(self, account_id: int) -> float:
+        return self.db.get_account(account_id).get_balance()
+
+    def get_all_account(self) -> dict[int, models.Account]:
+        return self.db.get_all_accounts()
+
+    def get_history(self, account_id: int) -> list[models.Transaction]:
+        return self.db.get_account(account_id=account_id).get_history()
